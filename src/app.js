4db03c88ff9bd99dd8ac2b801ba6852dd17da70e
HackinwaleDev <hackinwale.developer@gmail.com>
2022-09-15 14:30:08 +0100

    feat:: Setup CORS to allow third-party access

--- src/app.js
+++ src/app.js
@@ -2,11 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 
 module.exports = (config) => {
   var app = express();
 
+  /* Setup CORS options */
+  const corsOption = {
+    origin: true,
+    methods: 'GET, POST, PUT, DELETE',
+    credentials: true,
+    exposedHeaders: ['x-auth-token']
+  }
+
+  app.use(cors(corsOption));
   app.use(logger('dev'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
